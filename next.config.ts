4678f2c1d41773b79c7a871ef1e38a7a0c4109df
Navya Jain <nj255@snu.edu.in>
2025-07-10 03:37:24 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // In a real app, you would probably want to restrict this to known URLs
+  // but for the purposes of a tutorial, this is fine.
+  //
+  // For more information, see:
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
+  devIndicators: {
+    allowedDevOrigins: [
+      '*.cloudworkstations.dev',
+      '*.firebasestudio.corp.google.com',
+    ]
+  },
 };
 
 export default nextConfig;
